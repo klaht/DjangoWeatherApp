e4eab5eafa2b85ea5f8b97979a3ee302847042c2
klaht <konsta.lahtela@gmail.com>
2022-10-23 17:39:09 +0300

    Improved basic functionality

--- mysite/weather/urls.py
+++ mysite/weather/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('submit_city', views.submit_city)
 ]
--- mysite/weather/views.py
+++ mysite/weather/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from .forms import CityForm
 import requests
 import json
 
 api_key = "dd82f9fc45781a5307d640e53bfcee0d"
 base_url = "http://api.openweathermap.org/data/2.5/weather?"
-url = base_url + "appid=" + api_key + "&q=" + "Oulu"
+default_city = "Helsinki"
+url = base_url + "appid=" + api_key + "&q=" + default_city
 response = requests.get(url)
 x = response.json()
 
@@ -15,9 +17,41 @@ def index(request):
         y = x["main"]
 
     data = {
+        'city': default_city,
         'temperature': round(float(y["temp"]) - 273.15, 1),
         'pressure': float(y["pressure"]),
-        'humidity': float(y["humidity"]),
+        'humidity': float(y["humidity"])
     }
 
     return render(request, 'weather/index.html', data)
+
+
+def submit_city(request):
+
+    if request.method == 'GET':
+        form = CityForm(request.GET)
+
+        if form.is_valid():
+
+            city = form.cleaned_data.get("city")
+
+            url = base_url + "appid=" + api_key + "&q=" + city
+            response = requests.get(url)
+            x = response.json()
+
+            if x["cod"] != "404":
+                y = x["main"]
+            else:
+                return render(request, 'weather/error.html')
+
+
+            data = {
+                'city': city,
+                'temperature': round(float(y["temp"]) - 273.15, 1),
+                'pressure': float(y["pressure"]),
+                'humidity': float(y["humidity"])
+            }
+
+            return render(request, 'weather/index.html', data)
+
+    return HttpResponse("Error")
